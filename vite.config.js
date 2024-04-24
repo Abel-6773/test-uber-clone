d055841eac78b95a70321321a08ba70485d7c8d6
Abel-6773 <abeltsehaye14@gmail.com>
2024-04-24 20:30:39 +0200

    modified jsonfile

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import copy from "vite-plugin-copy";
+// import copy from "vite-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    copy({
-      targets: [
-        { src: "public/_redirects", dest: "." }, // Copy _redirects to the root of build output
-      ],
-    }),
+    // copy({
+    //   targets: [
+    //     { src: "public/_redirects", dest: "." }, // Copy _redirects to the root of build output
+    //   ],
+    // }),
   ],
 });
