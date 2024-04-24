efddd51f67eaaa85e603401d193576c6674951cd
Abel-6773 <abeltsehaye14@gmail.com>
2024-04-24 20:05:39 +0200

    added vite plugin copy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import copy from "vite-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: "public/_redirects", dest: "." }, // Copy _redirects to the root of build output
+      ],
+    }),
+  ],
+});
